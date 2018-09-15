540d097314297b14a0ab5f2b977364adad7f343e
Igor Pauli Quintaes <igor.quintaes@lambda3.com.br>
2018-09-15 15:16:15 -0300

    initial commit

---
+++ src/Deluge.Calculator.MoveSet/AttackAnalyzer.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Deluge.Calculator.MoveSet
+{
+    public static class Calculator
+    {
+        private const double _strong = 2;
+        private const double _normal = 1;
+        private const double _weak = 0.5;
+        private const double _immune = 0;
+
+        public static List<KeyValuePair<AttackType, double>> Calculate(AttackType enemyType, bool inverse = false)
+        {
+            var data = new List<KeyValuePair<AttackType, double>>();
+            var itensTochange = new List<KeyValuePair<AttackType, double>>();
+            data.AddRange(Enum.GetValues(typeof(AttackType)).OfType<AttackType>().Select(x => new KeyValuePair<AttackType, double>(x, _normal)));
+
+            switch (enemyType)
+            {
+                case AttackType.normal:
+                    AddImuneTypes(itensTochange,
+                        AttackType.ghost);
+                    AddStrongTypes(itensTochange,
+                        AttackType.fighting);
+                    break;
+                case AttackType.fire:
+                    AddWeakTypes(itensTochange,
+                        AttackType.fire,
+                        AttackType.grass,
+                        AttackType.ice,
+                        AttackType.bug,
+                        AttackType.steel,
+                        AttackType.fairy);
+                    AddStrongTypes(itensTochange,
+                        AttackType.water,
+                        AttackType.ground,
+                        AttackType.rock);
+                    break;
+                case AttackType.water:
+                    AddWeakTypes(itensTochange,
+                        AttackType.fire,
+                        AttackType.water,
+                        AttackType.ice,
+                        AttackType.steel);
+                    AddStrongTypes(itensTochange,
+                        AttackType.electric,
+                        AttackType.grass);
+                    break;
+                case AttackType.electric:
+                    AddWeakTypes(itensTochange,
+                        AttackType.electric,
+                        AttackType.flying,
+                        AttackType.steel);
+                    AddStrongTypes(itensTochange,
+                        AttackType.ground);
+                    break;
+                case AttackType.grass:
+                    AddWeakTypes(itensTochange,
+                        AttackType.water,
+                        AttackType.electric,
+                        AttackType.grass,
+                        AttackType.ground);
+                    AddStrongTypes(itensTochange,
+                        AttackType.fire,
+                        AttackType.ice,
+                        AttackType.poison,
+                        AttackType.flying,
+                        AttackType.bug);
+                    break;
+                case AttackType.ice:
+                    AddWeakTypes(itensTochange,
+                        AttackType.ice);
+                    AddStrongTypes(itensTochange,
+                        AttackType.fire,
+                        AttackType.fighting,
+                        AttackType.rock,
+                        AttackType.steel);
+                    break;
+                case AttackType.fighting:
+                    AddWeakTypes(itensTochange,
+                        AttackType.bug,
+                        AttackType.rock,
+                        AttackType.dark);
+                    AddStrongTypes(itensTochange,
+                        AttackType.flying,
+                        AttackType.psychic,
+                        AttackType.fairy);
+                    break;
+                case AttackType.poison:
+                    AddWeakTypes(itensTochange,
+                        AttackType.grass,
+                        AttackType.fighting,
+                        AttackType.poison,
+                        AttackType.bug,
+                        AttackType.fairy);
+                    AddStrongTypes(itensTochange,
+                        AttackType.ground,
+                        AttackType.psychic);
+                    break;
+                case AttackType.ground:
+                    AddImuneTypes(itensTochange,
+                        AttackType.electric);
+                    AddWeakTypes(itensTochange,
+                        AttackType.poison,
+                        AttackType.rock);
+                    AddStrongTypes(itensTochange,
+                        AttackType.water,
+                        AttackType.grass,
+                        AttackType.ice);
+                    break;
+                case AttackType.flying:
+                    AddImuneTypes(itensTochange,
+                        AttackType.ground);
+                    AddWeakTypes(itensTochange,
+                        AttackType.grass,
+                        AttackType.fighting,
+                        AttackType.bug);
+                    AddStrongTypes(itensTochange,
+                        AttackType.electric,
+                        AttackType.ice,
+                        AttackType.rock);
+                    break;
+                case AttackType.psychic:
+                    AddWeakTypes(itensTochange,
+                        AttackType.fighting,
+                        AttackType.psychic);
+                    AddStrongTypes(itensTochange,
+                        AttackType.bug,
+                        AttackType.ghost,
+                        AttackType.dark);
+                    break;
+                case AttackType.bug:
+                    AddWeakTypes(itensTochange,
+                        AttackType.grass,
+                        AttackType.fighting,
+                        AttackType.ground);
+                    AddStrongTypes(itensTochange,
+                        AttackType.fire,
+                        AttackType.flying,
+                        AttackType.rock);
+                    break;
+                case AttackType.rock:
+                    AddWeakTypes(itensTochange,
+                        AttackType.normal,
+                        AttackType.fire,
+                        AttackType.poison,
+                        AttackType.flying);
+                    AddStrongTypes(itensTochange,
+                        AttackType.water,
+                        AttackType.grass,
+                        AttackType.fighting,
+                        AttackType.ground,
+                        AttackType.steel);
+                    break;
+                case AttackType.ghost:
+                    AddImuneTypes(itensTochange,
+                        AttackType.normal,
+                        AttackType.fighting);
+                    AddWeakTypes(itensTochange,
+                        AttackType.poison,
+                        AttackType.bug);
+                    AddStrongTypes(itensTochange,
+                        AttackType.ghost,
+                        AttackType.dark);
+                    break;
+                case AttackType.dragon:
+                    AddWeakTypes(itensTochange,
+                        AttackType.fire,
+                        AttackType.water,
+                        AttackType.electric,
+                        AttackType.grass);
+                    AddStrongTypes(itensTochange,
+                        AttackType.ice,
+                        AttackType.dragon,
+                        AttackType.fairy);
+                    break;
+                case AttackType.dark:
+                    AddImuneTypes(itensTochange,
+                        AttackType.psychic);
+                    AddWeakTypes(itensTochange,
+                        AttackType.ghost,
+                        AttackType.dark);
+                    AddStrongTypes(itensTochange,
+                        AttackType.fighting,
+                        AttackType.bug,
+                        AttackType.fairy);
+                    break;
+                case AttackType.steel:
+                    AddImuneTypes(itensTochange,
+                        AttackType.poison);
+                    AddWeakTypes(itensTochange,
+                        AttackType.normal,
+                        AttackType.grass,
+                        AttackType.ice,
+                        AttackType.flying,
+                        AttackType.psychic,
+                        AttackType.bug,
+                        AttackType.rock,
+                        AttackType.dragon,
+                        AttackType.steel,
+                        AttackType.fairy);
+                    AddStrongTypes(itensTochange,
+                        AttackType.fire,
+                        AttackType.fighting,
+                        AttackType.ground);
+                    break;
+                case AttackType.fairy:
+                    AddImuneTypes(itensTochange,
+                        AttackType.dragon);
+                    AddWeakTypes(itensTochange,
+                        AttackType.fighting,
+                        AttackType.bug,
+                        AttackType.dark);
+                    AddStrongTypes(itensTochange,
+                        AttackType.poison,
+                        AttackType.steel);
+                    break;
+            }
+
+            return ChangeItems(data, itensTochange, inverse);
+        }
+
+        public static List<KeyValuePair<AttackType, double>> Calculate(AttackType enemyFirstType, AttackType enemySecondType, bool inverse = false)
+        {
+            if (enemyFirstType == enemySecondType)
+                return Calculate(enemyFirstType, inverse);
+
+            var enemyAttacksFirstType = Calculate(enemyFirstType, inverse);
+            var enemyAttacksSecondType = Calculate(enemySecondType, inverse);
+
+            var result = new List<KeyValuePair<AttackType, double>>();
+            for (var i = 0; i < enemyAttacksFirstType.Count; i++)
+            {
+                result.Add(new KeyValuePair<AttackType, double>(
+                    enemyAttacksFirstType[i].Key,
+                    enemyAttacksFirstType[i].Value * enemyAttacksSecondType[i].Value));
+            }
+
+            return result;
+        }
+
+        public static Attack GetBestAttack(List<Attack> moves, List<KeyValuePair<AttackType, double>> attackMetrics)
+        {
+            var strongestAttack = new KeyValuePair<Attack, double>(moves.First(), 0);
+            foreach (var move in moves)
+            {
+                var power = attackMetrics.Single(x => x.Key == move.Type).Value * move.Power;
+                if (power > strongestAttack.Value)
+                    strongestAttack = new KeyValuePair<Attack, double>(move, power);
+            }
+
+            return strongestAttack.Key;
+        }
+
+        public static List<Attack> GetBestAttackAllTypes(List<Attack> allMoves, List<KeyValuePair<AttackType, double>> attackMetrics)
+        {
+            return allMoves
+                .Where(x => x.Power < 200)
+                .Select(x => new Attack(x.Name, x.Type.ToString(), Convert.ToInt32(x.Power * attackMetrics.First(y => y.Key == x.Type).Value)))
+                .ToList();
+        }
+
+        private static void AddWeakTypes(List<KeyValuePair<AttackType, double>> data, params AttackType[] types) =>
+            data.AddRange(types.Select(x => new KeyValuePair<AttackType, double>(x, _weak)));
+
+        private static void AddStrongTypes(List<KeyValuePair<AttackType, double>> data, params AttackType[] types) =>
+            data.AddRange(types.Select(x => new KeyValuePair<AttackType, double>(x, _strong)));
+
+        private static void AddImuneTypes(List<KeyValuePair<AttackType, double>> data, params AttackType[] types) =>
+            data.AddRange(types.Select(x => new KeyValuePair<AttackType, double>(x, _immune)));
+
+        private static List<KeyValuePair<AttackType, double>> ChangeItems(
+            List<KeyValuePair<AttackType, double>> data,
+            List<KeyValuePair<AttackType, double>> itensTochange,
+            bool inverse)
+        {
+            foreach (var itemToChange in itensTochange)
+            {
+                var i = data.FindIndex(n => n.Key.Equals(itemToChange.Key));
+                if (inverse && (itemToChange.Value == _weak || itemToChange.Value == _immune))
+                    data[i] = new KeyValuePair<AttackType, double>(itemToChange.Key, _strong);
+                else if (inverse && itemToChange.Value == _strong)
+                    data[i] = new KeyValuePair<AttackType, double>(itemToChange.Key, _weak);
+                else
+                    data[i] = itemToChange;
+            }
+
+            return data;
+        }
+    }
+}
---
+++ src/Deluge.Calculator.MoveSet/Program.cs
@@ -0,0 +1,154 @@
+﻿using Combinatorics.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Deluge.Calculator.MoveSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = AttackDatabase.ObtainData();
+
+            Console.WriteLine("Type your enemy team type, splitting a dual-type pokémon using comma.");
+            Console.WriteLine("Press enter to input a new pokémon and/or 'start' if the pokémon quantity is lower than 6.");
+            Console.WriteLine("You can also enter a negative pokemon");
+            Console.WriteLine("Example.");
+            Console.WriteLine("Dragon, Water");
+            Console.WriteLine("Fire");
+            Console.WriteLine("Grass, Ice, Negative");
+            Console.WriteLine("start");
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("Type your enemy team type here:");
+
+            var enemyTypesList = new List<PokemonAttacks>();
+
+            for (var count = 0; count < 6; count++)
+            {
+                var pokemonAttacks = new PokemonAttacks();
+
+                var type = Console.ReadLine();
+
+                if (type.ToLower() == "start")
+                    break;
+
+                pokemonAttacks.Inverse = type.Split(',').Any(x => x.Trim().ToLower() == "negative");
+                pokemonAttacks.Primary = Enum.Parse<AttackType>(type.Split(',').First(x => x.Trim().ToLower() != "negative"));
+                pokemonAttacks.Secondary = Enum.Parse<AttackType>(type.Split(',').Last(x => x.Trim().ToLower() != "negative"));
+
+                enemyTypesList.Add(pokemonAttacks);
+            }
+
+            Console.WriteLine("Is a inverse battle? (true / false):");
+            var isInverse = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Calculating the better movelist...");
+
+            var betterAttacksAll = new List<Tuple<PokemonAttacks, List<Attack>>>();
+            foreach (var enemyTypes in enemyTypesList)
+            {
+                var powerVariationList = Calculator.Calculate(enemyTypes.Primary, enemyTypes.Secondary, isInverse);
+                var resultBestAttackBatch = Calculator.GetBestAttackAllTypes(data, powerVariationList).OrderByDescending(x => x.Power).ToList();
+                betterAttacksAll.Add(new Tuple<PokemonAttacks, List<Attack>>(enemyTypes, resultBestAttackBatch));
+            }
+
+            var extraSlots = betterAttacksAll.Count - 4;
+            extraSlots = extraSlots < 0 ? 0 : extraSlots;
+
+            var combinations = GetKCombs(betterAttacksAll, extraSlots + 1);
+            if (combinations.First().Count() == 1)
+            {
+                var allCombinations = UpdateRemainingCombinations(betterAttacksAll);
+                PrintResult(allCombinations);
+                return;
+
+            }
+            if (combinations.First().Count() == 2)
+            {
+                var strongestCombination = GetStrongestCombination(combinations);
+                var allCombinations = new List<Tuple<List<PokemonAttacks>, Attack>>();
+                allCombinations.Add(strongestCombination);
+                allCombinations.AddRange(UpdateRemainingCombinations(betterAttacksAll.Where(x => !strongestCombination.Item1.Contains(x.Item1))));
+                PrintResult(allCombinations);
+                return;
+            }
+            if (combinations.First().Count() == 3)
+            {
+
+                var strongestTripleCombination = GetStrongestCombination(combinations);
+
+                var dualCombinations = GetKCombs(betterAttacksAll, 2);
+                var strongestDualCombination1 = GetStrongestCombination(dualCombinations);
+
+                var dualCombinationsWithoutTop = GetKCombs(betterAttacksAll.Where(x => !strongestDualCombination1.Item1.Contains(x.Item1)), 2);
+                var strongestDualCombination2 = GetStrongestCombination(dualCombinationsWithoutTop);
+
+                var allCombinations = new List<Tuple<List<PokemonAttacks>, Attack>>();
+                if (strongestTripleCombination.Item2.Power < strongestDualCombination2.Item2.Power + strongestDualCombination1.Item2.Power)
+                {
+                    allCombinations.Add(strongestDualCombination1);
+                    allCombinations.Add(strongestDualCombination2);
+                    allCombinations.AddRange(UpdateRemainingCombinations(betterAttacksAll.Where(x =>
+                        !strongestDualCombination1.Item1.Contains(x.Item1) &&
+                        !strongestDualCombination2.Item1.Contains(x.Item1))));
+                    PrintResult(allCombinations);
+                    return;
+                }
+
+                allCombinations.Add(strongestTripleCombination);
+                allCombinations.AddRange(UpdateRemainingCombinations(betterAttacksAll.Where(x =>
+                    !strongestTripleCombination.Item1.Contains(x.Item1))));
+                PrintResult(allCombinations);
+            }
+        }
+
+        public static Tuple<List<PokemonAttacks>, Attack> GetStrongestCombination(IEnumerable<IEnumerable<Tuple<PokemonAttacks, List<Attack>>>> kCombinations)
+        {
+            var strongestCombination = new Tuple<List<PokemonAttacks>, Attack>(new List<PokemonAttacks>(), new Attack());
+            foreach (var combination in kCombinations)
+            {
+                for (var count = 0; count < combination.First().Item2.Count; count++)
+                {
+                    var attacks = combination.ToList().Skip(1)
+                        .Select(x => x.Item2.First(y => y.Name == combination.ToList()[0].Item2[count].Name && y.Type == combination.ToList()[0].Item2[count].Type))
+                        .Append(combination.ToList()[0].Item2[count]);
+
+                    var atkResult = new Attack(attacks.First().Name, attacks.First().Type.ToString(), attacks.Sum(x => x.Power) / 3);
+
+                    if (atkResult.Power > strongestCombination.Item2.Power && attacks.All(x => x.Power > 0))
+                    {
+                        strongestCombination = new Tuple<List<PokemonAttacks>, Attack>(combination.ToList().Select(x => x.Item1).ToList(), atkResult);
+                    }
+                }
+            }
+
+            return strongestCombination;
+        }
+
+        public static IEnumerable<Tuple<List<PokemonAttacks>, Attack>> UpdateRemainingCombinations(IEnumerable<Tuple<PokemonAttacks, List<Attack>>> remainingEnemies) =>
+            remainingEnemies.Select(x => new Tuple<List<PokemonAttacks>, Attack>(
+                item1: new List<PokemonAttacks>() { x.Item1 },
+                item2: x.Item2.OrderByDescending(y => y.Power).First()));
+
+        public static IEnumerable<IEnumerable<Tuple<PokemonAttacks, List<Attack>>>> GetKCombs(IEnumerable<Tuple<PokemonAttacks, List<Attack>>> list, int length)
+        {
+            if (length < 1) throw new ArgumentOutOfRangeException(nameof(length));
+            if (length == 1) return list.Select(t => new Tuple<PokemonAttacks, List<Attack>>[] { t });
+
+            return new Combinations<Tuple<PokemonAttacks, List<Attack>>>(list.ToList(), length).ToList();
+        }
+
+        public static void PrintResult(IEnumerable<Tuple<List<PokemonAttacks>, Attack>> results)
+        {
+            foreach (var result in results)
+            {
+                Console.WriteLine(string.Join(" & ", result.Item1.Select(x => $"{x.Primary.ToString()} {x.Secondary.ToString()} {(x.Inverse ? "Negative" : "")}")));
+                Console.WriteLine($"{result.Item2.Name}, of type {result.Item2.Type} - Power variation: {result.Item2.Power}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
